75daa31f9205c26666f886c9a9ea1e1d56788239
Deeptha02 <srimandeep02@gmail.com>
2025-02-14 16:33:29 +0530

    Added startup code and modified platform.txt for riscv support

--- cores/arduino/startup.c
+++ cores/arduino/startup.c
@@ -1,301 +1,221 @@
-/*
-  Copyright (c) 2015 Arduino LLC.  All right reserved.
+/*******************************************************************************
+ * Project           		 : shakti devt board
+ * Name of the file              : start.S 
+ * Brief description             : first instruction to execute in an application
+ * Name of Author                : Abhinav Ramnath & Sathya Narayanan N
+ * Email id                      : abhinavramnath13@gmail.com & sathya281@gmail.com
 
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
+ Copyright (C) 2019  IIT Madras. All rights reserved.
 
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-  See the GNU Lesser General Public License for more details.
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
 
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+********************************************************************************/
+/**
+@file start.S
+@details first instruction to execute in an application is here. 
+ Registers are initialized, trap entry is setup here.
 */
 
-#include <samd.h>
-#include "variant.h"
+#include "defines.h"
 
-#include <stdio.h>
+.section ".text.init"
+.globl _start
 
-/**
- * \brief SystemInit() configures the needed clocks and according Flash Read Wait States.
- * At reset:
- * - OSC8M clock source is enabled with a divider by 8 (1MHz).
- * - Generic Clock Generator 0 (GCLKMAIN) is using OSC8M as source.
- * We need to:
- * 1) Enable XOSC32K clock (External on-board 32.768Hz oscillator), will be used as DFLL48M reference.
- * 2) Put XOSC32K as source of Generic Clock Generator 1
- * 3) Put Generic Clock Generator 1 as source for Generic Clock Multiplexer 0 (DFLL48M reference)
- * 4) Enable DFLL48M clock
- * 5) Switch Generic Clock Generator 0 to DFLL48M. CPU will run at 48MHz.
- * 6) Modify PRESCaler value of OSCM to have 8MHz
- * 7) Put OSC8M as source for Generic Clock Generator 3
- */
-// Constants for Clock generators
-#define GENERIC_CLOCK_GENERATOR_MAIN      (0u)
-#define GENERIC_CLOCK_GENERATOR_XOSC32K   (1u)
-#define GENERIC_CLOCK_GENERATOR_OSC32K    (1u)
-#define GENERIC_CLOCK_GENERATOR_OSCULP32K (2u) /* Initialized at reset for WDT */
-#define GENERIC_CLOCK_GENERATOR_OSC8M     (3u)
-// Constants for Clock multiplexers
-#define GENERIC_CLOCK_MULTIPLEXER_DFLL48M (0u)
-
-void SystemInit( void )
-{
-  /* Set 1 Flash Wait State for 48MHz, cf tables 20.9 and 35.27 in SAMD21 Datasheet */
-  NVMCTRL->CTRLB.bit.RWS = NVMCTRL_CTRLB_RWS_HALF_Val ;
-
-  /* Turn on the digital interface clock */
-  PM->APBAMASK.reg |= PM_APBAMASK_GCLK ;
+_start:
+#set up trap entry point
+la t0, trap_entry
+csrw mtvec, t0
 
 
-#if defined(CRYSTALLESS)
+#disable interrupts
+li      t0, 0xb
+csrrc   zero, mstatus, t0 #disable global interrupt for all modes
+li      t0, 0x888
+csrrc   zero, mie, t0  # disable machine level interrupt
 
-  /* ----------------------------------------------------------------------------------------------
-   * 1) Enable OSC32K clock (Internal 32.768Hz oscillator)
-   */
+#Enable FP
+li a0, MSTATUS_FS & (MSTATUS_FS >> 1)
+csrs mstatus, a0
+csrwi fcsr, 0
 
-  uint32_t calib = (*((uint32_t *) FUSES_OSC32K_CAL_ADDR) & FUSES_OSC32K_CAL_Msk) >> FUSES_OSC32K_CAL_Pos;
 
-  SYSCTRL->OSC32K.reg = SYSCTRL_OSC32K_CALIB(calib) |
-                        SYSCTRL_OSC32K_STARTUP( 0x6u ) | // cf table 15.10 of product datasheet in chapter 15.8.6
-                        SYSCTRL_OSC32K_EN32K |
-                        SYSCTRL_OSC32K_ENABLE;
+/*
+Disabled, so as to work in QEMU
+Enable if needed
+*/
 
-  while ( (SYSCTRL->PCLKSR.reg & SYSCTRL_PCLKSR_OSC32KRDY) == 0 ); // Wait for oscillator stabilization
+#if 0
+ #define PLICENABLE 0x0C002000
+ li t6, PLICENABLE
+ andi t3,t3,0
+ addi t3,t3,0
+ sb t3, 0(t6)
+ sb t3, 1(t6)
+ sb t3, 2(t6)
+ sb t3, 3(t6)
 
-#else // has crystal
-
-  /* ----------------------------------------------------------------------------------------------
-   * 1) Enable XOSC32K clock (External on-board 32.768Hz oscillator)
-   */
-  SYSCTRL->XOSC32K.reg = SYSCTRL_XOSC32K_STARTUP( 0x6u ) | /* cf table 15.10 of product datasheet in chapter 15.8.6 */
-                         SYSCTRL_XOSC32K_XTALEN | SYSCTRL_XOSC32K_EN32K ;
-  SYSCTRL->XOSC32K.bit.ENABLE = 1 ; /* separate call, as described in chapter 15.6.3 */
-
-  while ( (SYSCTRL->PCLKSR.reg & SYSCTRL_PCLKSR_XOSC32KRDY) == 0 )
-  {
-    /* Wait for oscillator stabilization */
-  }
-
+ #define PLICPRIORITY 0x0C000000
+ li t6, PLICPRIORITY
+ andi t3,t3,0
+ sb t3, 0(t6)
+ sb t3, 1(t6)
+ sb t3, 2(t6)
+ sb t3, 3(t6)
 #endif
 
-  /* Software reset the module to ensure it is re-initialized correctly */
-  /* Note: Due to synchronization, there is a delay from writing CTRL.SWRST until the reset is complete.
-   * CTRL.SWRST and STATUS.SYNCBUSY will both be cleared when the reset is complete, as described in chapter 13.8.1
-   */
-  GCLK->CTRL.reg = GCLK_CTRL_SWRST ;
+li  x1, 0
+li  x2, 0
+li  x3, 0
+li  x4, 0
+li  x5, 0
+li  x6, 0
+li  x7, 0
+li  x8, 0
+li  x9, 0
+li  x10,0
+li  x11,0
+li  x12,0
+li  x13,0
+li  x14,0
+li  x15,0
+li  x16,0
+li  x17,0
+li  x18,0
+li  x19,0
+li  x20,0
+li  x21,0
+li  x22,0
+li  x23,0
+li  x24,0
+li  x25,0
+li  x26,0
+li  x27,0
+li  x28,0
+li  x29,0
+li  x30,0
+li  x31,0
 
-  while ( (GCLK->CTRL.reg & GCLK_CTRL_SWRST) && (GCLK->STATUS.reg & GCLK_STATUS_SYNCBUSY) )
-  {
-    /* Wait for reset to complete */
-  }
+# initialize global pointer
+.option push
+.option norelax
+la gp, __global_pointer$
+.option pop
 
-  /* ----------------------------------------------------------------------------------------------
-   * 2) Put XOSC32K as source of Generic Clock Generator 1
-   */
-  GCLK->GENDIV.reg = GCLK_GENDIV_ID( GENERIC_CLOCK_GENERATOR_XOSC32K ) ; // Generic Clock Generator 1
+init_ramcode:
+    /* init sdata section */
+    la	a0, __ram_code_start
+    la	a1, __ram_code_end-4 /* section end is actually the start of the next section */
+    la	a2, _la_ramcode
+  1:
+    lw    a3, 0(a2)
+    sw		a3, 0(a0)
+    bgeu	a0, a1, init_data
+    addi	a0, a0, 4
+    addi  a2, a2, 4
+    j	        1b
 
-  while ( GCLK->STATUS.reg & GCLK_STATUS_SYNCBUSY )
-  {
-    /* Wait for synchronization */
-  }
+init_data:
+    /* init data section */
+    la	a0, __data_start
+    la	a1, __data_end-4 /* section end is actually the start of the next section */
+    la	a2, _la_data
+  1:
+    lw    a3, 0(a2)
+    sw		a3, 0(a0)
+    bgeu	a0, a1, init_sdata
+    addi	a0, a0, 4
+    addi  a2, a2, 4
+    j	        1b
 
-  /* Write Generic Clock Generator 1 configuration */
-  GCLK->GENCTRL.reg = GCLK_GENCTRL_ID( GENERIC_CLOCK_GENERATOR_OSC32K ) | // Generic Clock Generator 1
-#if defined(CRYSTALLESS)
-                      GCLK_GENCTRL_SRC_OSC32K | // Selected source is Internal 32KHz Oscillator
-#else
-                      GCLK_GENCTRL_SRC_XOSC32K | // Selected source is External 32KHz Oscillator
-#endif
-//                      GCLK_GENCTRL_OE | // Output clock to a pin for tests
-                      GCLK_GENCTRL_GENEN ;
+init_sdata:
+    /* init sdata section */
+    la	a0, __sdata_start
+    la	a1, __sdata_end-4 /* section end is actually the start of the next section */
+    la	a2, _la_sdata
+  1:
+    lw    a3, 0(a2)
+    sw		a3, 0(a0)
+    bgeu	a0, a1, init_bss
+    addi	a0, a0, 4
+    addi  a2, a2, 4
+    j	        1b
 
-  while ( GCLK->STATUS.reg & GCLK_STATUS_SYNCBUSY )
-  {
-    /* Wait for synchronization */
-  }
+init_bss:
+    /* init bss section */
+    la	a0, __bss_start
+    la	a1, __bss_end-4 /* section end is actually the start of the next section */
+  1:
+    sw		x0, 0(a0)
+    bgeu	a0, a1, init_sbss
+    addi	a0, a0, 4
+    j	        1b
 
-  /* ----------------------------------------------------------------------------------------------
-   * 3) Put Generic Clock Generator 1 as source for Generic Clock Multiplexer 0 (DFLL48M reference)
-   */
-  GCLK->CLKCTRL.reg = GCLK_CLKCTRL_ID( GENERIC_CLOCK_MULTIPLEXER_DFLL48M ) | // Generic Clock Multiplexer 0
-                      GCLK_CLKCTRL_GEN_GCLK1 | // Generic Clock Generator 1 is source
-                      GCLK_CLKCTRL_CLKEN ;
+init_sbss:
+    /* init bss section */
+    la	a0, __sbss_start
+    la	a1, __sbss_end-4 /* section end is actually the start of the next section */
+   1:
+    sw		x0, 0(a0)
+    bgeu	a0, a1, 1f
+    addi	a0, a0, 4
+    j	        1b
 
-  while ( GCLK->STATUS.reg & GCLK_STATUS_SYNCBUSY )
-  {
-    /* Wait for synchronization */
-  }
+1:
+# initialize stack pointer
+.option push
+.option norelax
+la sp, __stack_pointer$
+.option pop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+la  tp, _end + 63
+and tp, tp, -64
 
-  /* ----------------------------------------------------------------------------------------------
-   * 4) Enable DFLL48M clock
-   */
+fence.i
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+j _init_
 
-  /* DFLL Configuration in Closed Loop mode, cf product datasheet chapter 15.6.7.1 - Closed-Loop Operation */
+1:
+  j 1b
 
-  /* Remove the OnDemand mode, Bug http://avr32.icgroup.norway.atmel.com/bugzilla/show_bug.cgi?id=9905 */
-  SYSCTRL->DFLLCTRL.reg = SYSCTRL_DFLLCTRL_ENABLE;
+.section ".tdata.begin"
+.globl _tdata_begin
+_tdata_begin:
 
-  while ( (SYSCTRL->PCLKSR.reg & SYSCTRL_PCLKSR_DFLLRDY) == 0 )
-  {
-    /* Wait for synchronization */
-  }
+.section ".tdata.end"
+.globl _tdata_end
+_tdata_end:
 
-  SYSCTRL->DFLLMUL.reg = SYSCTRL_DFLLMUL_CSTEP( 31 ) | // Coarse step is 31, half of the max value
-                         SYSCTRL_DFLLMUL_FSTEP( 511 ) | // Fine step is 511, half of the max value
-                         SYSCTRL_DFLLMUL_MUL( (VARIANT_MCK + VARIANT_MAINOSC/2) / VARIANT_MAINOSC ) ; // External 32KHz is the reference
-
-  while ( (SYSCTRL->PCLKSR.reg & SYSCTRL_PCLKSR_DFLLRDY) == 0 )
-  {
-    /* Wait for synchronization */
-  }
-
-#if defined(CRYSTALLESS)
-
-  #define NVM_SW_CALIB_DFLL48M_COARSE_VAL 58
-
-  // Turn on DFLL
-  uint32_t coarse =( *((uint32_t *)(NVMCTRL_OTP4) + (NVM_SW_CALIB_DFLL48M_COARSE_VAL / 32)) >> (NVM_SW_CALIB_DFLL48M_COARSE_VAL % 32) )
-                   & ((1 << 6) - 1);
-  if (coarse == 0x3f) {
-    coarse = 0x1f;
-  }
-  // TODO(tannewt): Load this value from memory we've written previously. There
-  // isn't a value from the Atmel factory.
-  uint32_t fine = 0x1ff;
-
-  SYSCTRL->DFLLVAL.bit.COARSE = coarse;
-  SYSCTRL->DFLLVAL.bit.FINE = fine;
-  /* Write full configuration to DFLL control register */
-  SYSCTRL->DFLLMUL.reg = SYSCTRL_DFLLMUL_CSTEP( 0x1f / 4 ) | // Coarse step is 31, half of the max value
-                         SYSCTRL_DFLLMUL_FSTEP( 10 ) |
-                         SYSCTRL_DFLLMUL_MUL( (48000) ) ;
-
-  SYSCTRL->DFLLCTRL.reg = 0;
-
-  while ( (SYSCTRL->PCLKSR.reg & SYSCTRL_PCLKSR_DFLLRDY) == 0 )
-  {
-    /* Wait for synchronization */
-  }
-
-  SYSCTRL->DFLLCTRL.reg =  SYSCTRL_DFLLCTRL_MODE |
-                           SYSCTRL_DFLLCTRL_CCDIS |
-                           SYSCTRL_DFLLCTRL_USBCRM | /* USB correction */
-                           SYSCTRL_DFLLCTRL_BPLCKC;
-
-  while ( (SYSCTRL->PCLKSR.reg & SYSCTRL_PCLKSR_DFLLRDY) == 0 )
-  {
-    /* Wait for synchronization */
-  }
-
-  /* Enable the DFLL */
-  SYSCTRL->DFLLCTRL.reg |= SYSCTRL_DFLLCTRL_ENABLE ;
-
-#else   // has crystal
-
-  /* Write full configuration to DFLL control register */
-  SYSCTRL->DFLLCTRL.reg |= SYSCTRL_DFLLCTRL_MODE | /* Enable the closed loop mode */
-                           SYSCTRL_DFLLCTRL_WAITLOCK |
-                           SYSCTRL_DFLLCTRL_QLDIS ; /* Disable Quick lock */
-
-  while ( (SYSCTRL->PCLKSR.reg & SYSCTRL_PCLKSR_DFLLRDY) == 0 )
-  {
-    /* Wait for synchronization */
-  }
-
-  /* Enable the DFLL */
-  SYSCTRL->DFLLCTRL.reg |= SYSCTRL_DFLLCTRL_ENABLE ;
-
-  while ( (SYSCTRL->PCLKSR.reg & SYSCTRL_PCLKSR_DFLLLCKC) == 0 ||
-          (SYSCTRL->PCLKSR.reg & SYSCTRL_PCLKSR_DFLLLCKF) == 0 )
-  {
-    /* Wait for locks flags */
-  }
-
-#endif
-
-  while ( (SYSCTRL->PCLKSR.reg & SYSCTRL_PCLKSR_DFLLRDY) == 0 )
-  {
-    /* Wait for synchronization */
-  }
-
-  /* ----------------------------------------------------------------------------------------------
-   * 5) Switch Generic Clock Generator 0 to DFLL48M. CPU will run at 48MHz.
-   */
-  GCLK->GENDIV.reg = GCLK_GENDIV_ID( GENERIC_CLOCK_GENERATOR_MAIN ) ; // Generic Clock Generator 0
-
-  while ( GCLK->STATUS.reg & GCLK_STATUS_SYNCBUSY )
-  {
-    /* Wait for synchronization */
-  }
-
-  /* Write Generic Clock Generator 0 configuration */
-  GCLK->GENCTRL.reg = GCLK_GENCTRL_ID( GENERIC_CLOCK_GENERATOR_MAIN ) | // Generic Clock Generator 0
-                      GCLK_GENCTRL_SRC_DFLL48M | // Selected source is DFLL 48MHz
-//                      GCLK_GENCTRL_OE | // Output clock to a pin for tests
-                      GCLK_GENCTRL_IDC | // Set 50/50 duty cycle
-                      GCLK_GENCTRL_GENEN ;
-
-  while ( GCLK->STATUS.reg & GCLK_STATUS_SYNCBUSY )
-  {
-    /* Wait for synchronization */
-  }
-
-  /* ----------------------------------------------------------------------------------------------
-   * 6) Modify PRESCaler value of OSC8M to have 8MHz
-   */
-  SYSCTRL->OSC8M.bit.PRESC = SYSCTRL_OSC8M_PRESC_0_Val ;  //CMSIS 4.5 changed the prescaler defines
-  SYSCTRL->OSC8M.bit.ONDEMAND = 0 ;
-
-  /* ----------------------------------------------------------------------------------------------
-   * 7) Put OSC8M as source for Generic Clock Generator 3
-   */
-  GCLK->GENDIV.reg = GCLK_GENDIV_ID( GENERIC_CLOCK_GENERATOR_OSC8M ) ; // Generic Clock Generator 3
-
-  /* Write Generic Clock Generator 3 configuration */
-  GCLK->GENCTRL.reg = GCLK_GENCTRL_ID( GENERIC_CLOCK_GENERATOR_OSC8M ) | // Generic Clock Generator 3
-                      GCLK_GENCTRL_SRC_OSC8M | // Selected source is RC OSC 8MHz (already enabled at reset)
-//                      GCLK_GENCTRL_OE | // Output clock to a pin for tests
-                      GCLK_GENCTRL_GENEN ;
-
-  while ( GCLK->STATUS.reg & GCLK_STATUS_SYNCBUSY )
-  {
-    /* Wait for synchronization */
-  }
-
-  /*
-   * Now that all system clocks are configured, we can set CPU and APBx BUS clocks.
-   * There values are normally the one present after Reset.
-   */
-  PM->CPUSEL.reg  = PM_CPUSEL_CPUDIV_DIV1 ;
-  PM->APBASEL.reg = PM_APBASEL_APBADIV_DIV1_Val ;
-  PM->APBBSEL.reg = PM_APBBSEL_APBBDIV_DIV1_Val ;
-  PM->APBCSEL.reg = PM_APBCSEL_APBCDIV_DIV1_Val ;
-
-  SystemCoreClock=VARIANT_MCK ;
-
-  /* ----------------------------------------------------------------------------------------------
-   * 8) Load ADC factory calibration values
-   */
-
-  // ADC Bias Calibration
-  uint32_t bias = (*((uint32_t *) ADC_FUSES_BIASCAL_ADDR) & ADC_FUSES_BIASCAL_Msk) >> ADC_FUSES_BIASCAL_Pos;
-
-  // ADC Linearity bits 4:0
-  uint32_t linearity = (*((uint32_t *) ADC_FUSES_LINEARITY_0_ADDR) & ADC_FUSES_LINEARITY_0_Msk) >> ADC_FUSES_LINEARITY_0_Pos;
-
-  // ADC Linearity bits 7:5
-  linearity |= ((*((uint32_t *) ADC_FUSES_LINEARITY_1_ADDR) & ADC_FUSES_LINEARITY_1_Msk) >> ADC_FUSES_LINEARITY_1_Pos) << 5;
-
-  ADC->CALIB.reg = ADC_CALIB_BIAS_CAL(bias) | ADC_CALIB_LINEARITY_CAL(linearity);
-
-  /*
-   * 9) Disable automatic NVM write operations
-   */
-  NVMCTRL->CTRLB.bit.MANW = 1;
-}
+.section ".tbss.end"
+.globl _tbss_end
+_tbss_end:
